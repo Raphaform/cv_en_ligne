6807aed1f334b0ec42d96cffa948d3815dc955b6
Raph <raphael.form@gmail.com>
2023-12-06 15:51:21 +0100

    JS animation ouverture overlay menu burger

--- script/script.js
+++ script/script.js
@@ -1,10 +1,15 @@
         var y = document.getElementById("overlay_burger");
         var x = document.getElementById("open_burger");
-        var z = document.getElementById("close_bar")
-        console.log('test');
+        var z = document.getElementById("close_bar");
+        var barre_burger = document.querySelectorAll(".burgericon");
+        console.log(barre_burger);
         x.addEventListener('click',function(){
-            y.classList.toggle('closeNav')
-            console.log('test')
+            y.classList.toggle('closeNav');
+            console.log('test');
+            barre_burger.forEach(div => {
+                this.classList.toggle('burger_open');
+            });
+            // barre_burger.classList.toggle('burger_open');
         })
     
     
