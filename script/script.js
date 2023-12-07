3bd69dbc295e9e4fb55ab2c539a0bc57cb910e2e
Raph <raphael.form@gmail.com>
2023-12-07 09:32:15 +0100

    cv format telephone css ajout de la section profil

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+        
         var y = document.getElementById("overlay_burger");
         var x = document.getElementById("open_burger");
         var z = document.getElementById("close_bar");
@@ -9,7 +10,6 @@
             barre_burger.forEach(div => {
                 this.classList.toggle('burger_open');
             });
-            // barre_burger.classList.toggle('burger_open');
         })
     
     
