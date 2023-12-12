d566d485a37ed02642fac845b35f96dc6d0e2144
Raph <raphael.form@gmail.com>
2023-12-12 13:10:36 +0100

    avancement js cv tel

--- script/script.js
+++ script/script.js
@@ -3,17 +3,29 @@
         var x = document.getElementById("open_burger");
         var z = document.getElementById("close_bar");
         var barre_burger = document.querySelectorAll(".burgericon");
-        console.log(barre_burger);
+
+        
+        var msg = document.getElementById("overlay_msg");
+        var bgblur = document.getElementById("tel_background")
+        var openmsg = document.getElementById("open_msg") 
+        var openmail = document.getElementById("contacter_mail") 
+        var opentel = document.getElementById("contacter_tel") 
+        var maillink = document.getElementById("lienmail") 
+        var tellink = document.getElementById("lientel") 
+
         x.addEventListener('click',function(){
             y.classList.toggle('closeNav');
-            console.log('test');
             barre_burger.forEach(div => {
                 this.classList.toggle('burger_open');
             });
         })
     
-        var tech1 = document.getElementById("technique_part1")
-        var tech2 = document.getElementById("technique_part2")
-        var tech3 = document.getElementById("technique_part3")
-    
 
+    function open_overlay_msg(){
+        msg.classList.toggle('overlay_msg');
+        bgblur.classList.toggle('tel_background');
+        openmail.classList.toggle('contacter_mail');
+        opentel.classList.toggle('contacter_tel');
+        tellink.classList.toggle('lientel');
+        maillink.classList.toggle('lienmail');
+    }
